46f2a5b8989c03b3e83fc9bafcbedc1cb4f366dc
Gopal S <gopalworkingid@gmail.com>
2021-06-09 13:27:42 +0530

    Transaction title added

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Transactions(props) {
     return(
         <div className="transaction-wrapper">
+            <div className="transaction-title">Transactions: </div><br />
             {
                 props.list.map(item => <>
                     <span>{item.time}</span> - <span>{item.amount}</span> - <span>{item.type}</span><br />
