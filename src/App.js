784adc0c5fe5581ce7a166cd4d7c87fcd2f4d143
Gopal S <gopalworkingid@gmail.com>
2021-06-09 11:54:53 +0530

    Project setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react'
+
+class App extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Here APP component</h1>
+            </div>
+        )
+    }
+}
+
+export default App
